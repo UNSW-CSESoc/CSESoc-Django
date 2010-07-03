2cb1b8dcfbd971b1e941cdaa8054f20a82497a97
Robert Massaioli <robertmassaioli@gmail.com>
2010-07-04 09:50:03 +1000

    Added a settings backdoor that lets you always log into the admin site.
        It is very dangerous and alot of care should be put into making sure
        that is is never accidentally activated on the real website.

--- auth/backends.py
+++ auth/backends.py
@@ -8,32 +8,41 @@ from csesoc import passwd_data
 
 class CSEBackend(ModelBackend):
    def authenticate(self, token=None):
-      import MySQLdb
-      import urllib
-      from django.contrib.auth.models import User
-      token = urllib.unquote(token)
-      token = token.decode('iso-8859-1')
-      # Check the token and return a User.
-      db = MySQLdb.connect(db='cse_auth', user=passwd_data.DB_USERNAME, passwd=passwd_data.DB_PASSWORD)
-      c = db.cursor()
-      c.execute('select `user` from `users` where `cookie` = %s;', (token,))
+        import MySQLdb
+        import urllib
+        from django.contrib.auth.models import User
+        if passwd_data.ADMIN_NO_LOGIN == False:
+            token = urllib.unquote(token)
+            token = token.decode('iso-8859-1')
+            # Check the token and return a User.
+            db = MySQLdb.connect(db='cse_auth', user=passwd_data.DB_USERNAME, passwd=passwd_data.DB_PASSWORD)
+            c = db.cursor()
+            c.execute('select `user` from `users` where `cookie` = %s;', (token,))
 
-      row = c.fetchone()
-      if row and row[0]:
-          username = row[0]
-      else:
-          return None
-      user, created = User.objects.get_or_create(username=username, 
-               defaults={'password' : 'get from cse',
-                         'is_staff' : False,
-                         'is_superuser' : False})
-      return user
+            row = c.fetchone()
+            if row and row[0]:
+              username = row[0]
+            else:
+              return None
+            user, created = User.objects.get_or_create(username=username, 
+                   defaults={'password' : 'get from cse',
+                             'is_staff' : False,
+                             'is_superuser' : False})
+            return user
+        else:
+            user, created = User.objects.get_or_create(username="test_admin",
+                   defaults={'password' : 'get from cse',
+                             'is_staff' : True,
+                             'is_superuser' : True})
+            return user
 
 def cse_login(request, next=None):
    if not request.user.is_authenticated():
       user = None
       if request.COOKIES.has_key('cseauth'):
          user = authenticate(token=request.COOKIES['cseauth'])
+      if passwd_data.ADMIN_NO_LOGIN:
+          user = authenticate()
       if user is not None:
          if user.is_active:
             login(request, user)
--- passwd_data.py
+++ passwd_data.py
@@ -4,3 +4,5 @@ DB_USERNAME = 'cse_auth'
 DB_PASSWORD = 'dummy_password'
 
 SETTINGS_SECRET_KEY = '0g$%0eu0flf(0o0n@k0al$h@0fo0@lk=s0obe0**@r+dupn00l'
+
+ADMIN_NO_LOGIN  = True
