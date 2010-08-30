9502182a9c4affbe5f189aa5838822866759170e
Xiao Di Guan <kiara@spawn.puxlit.net>
2010-08-30 21:02:09 +1000

    Improved the 'thedate' easter egg

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.shortcuts import get_object_or_404
@@ -10,8 +11,6 @@ def static(request, path):
    p = get_object_or_404(Static, slug=path.replace('/','_'))
    return render_to_response('static.html', { 'object' : p }, context_instance=RequestContext(request) )
 
-from os import popen
 def thedate(request):
-   thedate = ' '.join(popen("date").readlines())
-   return render_to_response('thedate.html', { 'd' : thedate }, context_instance=RequestContext(request) )
+   return render_to_response('thedate.html', { 'date' : datetime.now() }, context_instance=RequestContext(request) )
 
