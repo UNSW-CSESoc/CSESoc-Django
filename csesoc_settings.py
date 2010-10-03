8aeddacaec9224435be696ecb834e6f15b56383c
Robert Massaioli <robertmassaioli@gmail.com>
2010-10-03 11:00:15 +1100

    Added in a descriptive comment for a csesoc option.

--- csesoc_settings.py
+++ csesoc_settings.py
@@ -5,6 +5,9 @@ DB_PASSWORD = 'dummy_password'
 
 SETTINGS_SECRET_KEY = '0g$%0eu0flf(0o0n@k0al$h@0fo0@lk=s0obe0**@r+dupn00l'
 
+# Only ever set to true in Development, this will always be false on the live site because setting
+# this variable to true activates a back door that allows anyone access to the admin site without a
+# password or any form of verification.
 ADMIN_NO_LOGIN  = True
 
 # Turn this on in production but for the here and now it just lets you send emails to non standard
