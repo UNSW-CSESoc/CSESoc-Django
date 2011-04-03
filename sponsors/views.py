64eff7ce5e46563f2f68b36b082470984656c41b
Aditya Keswani <akeswani@cse.unsw.edu.au>
2011-04-03 15:40:52 +1000

    Adding changes to sponsors

--- sponsors/views.py
+++ sponsors/views.py
@@ -1,6 +1,17 @@
-from csesoc import settings
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_list_or_404
 from django.template import RequestContext
+from csesoc.sponsors.models import Sponsor
+from datetime import date
+from csesoc import settings
 
 def sponsors(request):
-  return render_to_response('sponsor.html', context_instance=RequestContext(request))
+    return render_to_response('sponsor.html', 
+                              {'allSponsors' : Sponsor.objects.order_by('amount_paid').filter(expiry_date__gte=date.today),
+                               'nav' : 'oursponsors',}, 
+                              context_instance = RequestContext(request))
+
+def sponsorsList(request):
+    return get_list_or_404(Sponsor.objects.order_by('amount_paid'), expiry_date__gte=date.today)
+    
+
+
