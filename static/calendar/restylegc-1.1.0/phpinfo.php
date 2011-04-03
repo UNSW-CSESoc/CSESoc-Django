65b26a4b5dc21e15ccdd3834ccce68cded47a6a6
Aditya Keswani <akeswani@cse.unsw.edu.au>
2011-04-03 14:45:53 +1000

    Adding calendar for frontend changes

---
+++ static/calendar/restylegc-1.1.0/phpinfo.php
@@ -0,0 +1,9 @@
+<?php
+/* 
+ * For debugging your PHP configuration.  
+ * Put this on your web site somewhere and access the page via your web browser.
+ * You will get version information, configuration settings, a list of installed modules, etc.
+ */
+phpinfo();
+?>
+
