e4f4a16cc3d05c3e65532c2945f77e92345184ae
Maxwell Swadling <maxwellswadling@gmail.com>
2011-05-07 23:18:39 +1000

    Added 3 column layout.
    Signed-off-by: Maxwell Swadling <maxwellswadling@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :load_sponsors
+  
+  private
+  def load_sponsors
+    @sponsors = Sponsor.visible
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  
+  # A page's title
+  def page_title(y)
+    y = "#{y} - " unless y.empty?
+    "#{y}CSESoc - UNSW Computer Science and Engineering Society"
+  end
+  
   # Link's to static content that may not exist
   def static_content_path(name)
     begin
@@ -7,4 +14,5 @@ module ApplicationHelper
       return root_path
     end
   end
+  
 end
