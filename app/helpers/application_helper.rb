ab6ac3aa205b3d16d4b3a425a7c13435e89c6de5
Maxwell Swadling <maxwellswadling@gmail.com>
2011-05-07 22:42:44 +1000

    Added full nav bar.
    Signed-off-by: Maxwell Swadling <maxwellswadling@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Link's to static content that may not exist
+  def static_content_path(name)
+    begin
+      return Static.find(name)
+    rescue
+      return root_path
+    end
+  end
 end
