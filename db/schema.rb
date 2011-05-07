2b159654d8359a77f66d3412f102716fb9854554
Maxwell Swadling <maxwellswadling@gmail.com>
2011-05-08 00:41:27 +0800

    Added Rails Admin.
    Signed-off-by: Maxwell Swadling <maxwellswadling@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110507160439) do
+ActiveRecord::Schema.define(:version => 20110507163906) do
 
   create_table "comments", :force => true do |t|
     t.integer  "suggestion_id"
@@ -43,6 +43,19 @@ ActiveRecord::Schema.define(:version => 20110507160439) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.string   "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_histories_on_item_and_table_and_month_and_year"
 
   create_table "slugs", :force => true do |t|
     t.string   "name"
