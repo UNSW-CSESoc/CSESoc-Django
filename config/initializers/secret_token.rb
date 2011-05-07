97fd546a3301f66229bc65ba5cb3c7fba3fe9327
Maxwell Swadling <maxwellswadling@gmail.com>
2011-05-07 19:34:31 +1000

    Initial project.
    Signed-off-by: Maxwell Swadling <maxwellswadling@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CSESocWebsite::Application.config.secret_token = '191a310a7defe174dbb15da9c7bc363621612f2da66b87e2b58da90e84d907c792dcea51c11d12a41121d40c51c5ea1cbb84e77c0af23be46453cdd953060ec1'
