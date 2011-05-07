28a6bee308d4c14ed983d465c7f2267337d567d2
Maxwell Swadling <maxwellswadling@gmail.com>
2011-05-07 21:27:12 +1000

    Added schema.
    Signed-off-by: Maxwell Swadling <maxwellswadling@gmail.com>

---
+++ db/schema.rb
@@ -0,0 +1,88 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20110507100554) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "suggestion_id"
+    t.integer  "user_id"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.datetime "time"
+    t.string   "location"
+    t.boolean  "registration_required"
+    t.string   "registration_email"
+    t.boolean  "volunteers_required"
+    t.string   "volunteers_email"
+    t.text     "description"
+    t.datetime "publish_date"
+    t.integer  "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "news_items", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "publish_date"
+    t.integer  "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "slugs", :force => true do |t|
+    t.string   "name"
+    t.integer  "sluggable_id"
+    t.integer  "sequence",                     :default => 1, :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "slugs", ["name", "sluggable_type", "sequence", "scope"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
+  add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
+
+  create_table "sponsors", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "website"
+    t.string   "alt_text"
+    t.integer  "amount_paid"
+    t.datetime "start_date"
+    t.datetime "expiry_date"
+    t.text     "html_override"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "statics", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "suggestions", :force => true do |t|
+    t.string   "subject"
+    t.text     "message"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
