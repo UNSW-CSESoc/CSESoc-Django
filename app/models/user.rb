9bde9f2510e84c5aeb984800b0af1460819653ff
Maxwell Swadling <maxwellswadling@gmail.com>
2011-05-08 17:30:35 +0800

    All users must have an unique cse_username.
    Signed-off-by: Maxwell Swadling <maxwellswadling@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   devise :token_authenticatable, :rememberable, :trackable
 
+  validates :cse_username, :presence => true, :uniqueness => true
   # Setup accessible (or protected) attributes for your model
   attr_accessible :remember_me
 end
