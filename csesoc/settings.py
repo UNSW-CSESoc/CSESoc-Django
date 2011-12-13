96dc8aba6474124ef146afef9b4a6218bae9da5e
Jayen <jayen@cse.unsw.edu.au>
2011-12-13 21:56:47 +1100

    cleaning up deprecated configuration of database

--- csesoc/settings.py
+++ csesoc/settings.py
@@ -22,12 +22,12 @@ MANAGERS = ADMINS
 
 FILE_UPLOAD_PERMISSIONS = 0644
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = os.path.join(PROJECT_PATH, 'db.sqlite3')           # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_PATH, 'db.sqlite3')
+    }
+}
 
 # Mail Settings
 SMTP_HOST = 'smtp.unsw.edu.au'
