13a3e3dd25aa547170cb8611a0ff596cd5b01377
Jayen <jayen@cse.unsw.edu.au>
2011-12-13 22:54:30 +1100

    more deprecations.  more caching.

--- csesoc/settings.py
+++ csesoc/settings.py
@@ -72,13 +72,14 @@ SECRET_KEY = csesoc_settings.SETTINGS_SECRET_KEY
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-    # 'django.template.loaders.eggs.load_template_source',
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.request', # we need this to provide the request variable to each template
